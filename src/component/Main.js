3e88e9261099ca943801a8e7c9febaa2360cc659
bwj59 <bwj59@naver.com>
2022-02-27 21:18:08 +0900

    pr

--- src/component/Main.js
+++ src/component/Main.js
@@ -4,279 +4,43 @@ import Fade from 'react-reveal/Fade';
 function Main(){
     return(
         <div className="backgcolor">
+
+
             <Fade bottom>
-                <h1>React Reveal</h1>
+                <div className="pt-5">
+                   <span className="font_deco0 text-box0">Thank You, </span>
+                   <span className="font_deco1 text-box1">Valtteri</span>
+                </div>
             </Fade>
+
             <Fade bottom>
-                <h1>React Reveal</h1>
+                <div className="pt-5">
+                    <img src="../main_img0.png" className="main_img"></img>
+                </div>
             </Fade>
             <Fade bottom>
-                <h1>React Reveal</h1>
+                <div className="pt-5">
+                    <img src="../main_img1.png" className="main_img"></img>
+                </div>
             </Fade>
             <Fade bottom>
-                <h1>React Reveal</h1>
+                <div className="pt-5">
+                    <img src="../main_img2.png" className="main_img"></img>
+                </div>
             </Fade>
             <Fade bottom>
-                <h1>React Reveal</h1>
+                <div className="pt-5">
+                    <img src="../main_img3.png" className="main_img"></img>
+                </div>
             </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
-            <Fade bottom>
-                <h1>React Reveal</h1>
-            </Fade>
+            <div className="spacing"></div>
         </div>      
     )
 }export default Main;
 
+
+//<figure class="swing">
+//  <img src="../main_img0.png" width="300px" ></img>
+//</figure>
 
 
